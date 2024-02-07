f33d0cf43d6661d09edf764c33744c37f3e56f6e
Qwergil <54988999+Sijel@users.noreply.github.com>
2024-02-07 19:40:24 +0400

    сonslution
    132

---
+++ UtilityTest/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace UtilityTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InfoBox = new System.Windows.Forms.TextBox();
+            this.ConclusionButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InfoBox
+            // 
+            this.InfoBox.Location = new System.Drawing.Point(-2, 48);
+            this.InfoBox.Multiline = true;
+            this.InfoBox.Name = "InfoBox";
+            this.InfoBox.Size = new System.Drawing.Size(280, 256);
+            this.InfoBox.TabIndex = 0;
+            // 
+            // ConclusionButton
+            // 
+            this.ConclusionButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ConclusionButton.Location = new System.Drawing.Point(-4, -1);
+            this.ConclusionButton.Name = "ConclusionButton";
+            this.ConclusionButton.Size = new System.Drawing.Size(140, 51);
+            this.ConclusionButton.TabIndex = 1;
+            this.ConclusionButton.Text = "Показать данные";
+            this.ConclusionButton.UseVisualStyleBackColor = true;
+            this.ConclusionButton.Click += new System.EventHandler(this.ConclusionButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitButton.Location = new System.Drawing.Point(132, -1);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(146, 51);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 301);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ConclusionButton);
+            this.Controls.Add(this.InfoBox);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InfoBox;
+        private System.Windows.Forms.Button ConclusionButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ UtilityTest/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UtilityTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConclusionButton_Click(object sender, EventArgs e)
+        {
+            // Создаем экземпляр класса ManagementObjectSearcher,
+            // указываем запрос для получения информации о видеокарте, процессоре и оперативной памяти
+            var videoCardSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+            var processorSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            var memorySearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+
+            StringBuilder sb = new StringBuilder();
+
+            // Обрабатываем информацию о видеокарте
+            foreach (var videoCard in videoCardSearcher.Get())
+            {
+                // Получаем свойства видеокарты
+                var videoCardName = (string)videoCard["Name"];
+
+                // Добавляем информацию о видеокарте в StringBuilder
+                sb.AppendLine($"Video Card: {videoCardName}");
+            }
+
+            // Обрабатываем информацию о процессоре
+            foreach (var processor in processorSearcher.Get())
+            {
+                // Получаем свойства процессора
+                var processorName = (string)processor["Name"];
+
+                // Добавляем информацию о процессоре в StringBuilder
+                sb.AppendLine($"Processor: {processorName}");
+            }
+
+            // Обрабатываем информацию об оперативной памяти
+            foreach (var memory in memorySearcher.Get())
+            {
+                // Получаем свойства оперативной памяти
+                var memoryCapacity = Convert.ToUInt64(memory["Capacity"]);
+                var memorySize = memoryCapacity / (1024 * 1024 * 1024); // Преобразуем к гигабайтам
+
+                // Добавляем информацию об оперативной памяти в StringBuilder
+                sb.AppendLine($"Memory: {memorySize}GB");
+            }
+
+            // Записываем результат в textBox1
+            InfoBox.Text = sb.ToString();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Закрыть приложение
+        }
+    }
+}
